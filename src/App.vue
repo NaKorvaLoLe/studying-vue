4c0348b4baea63781dd2d47a965b44aa10cfdd2a
Александр <cwotka@yandex.ru>
2025-07-17 00:48:17 +0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script>
+import User from "./components/User.vue";
+
 export default {
+  components: {
+    User,
+  },
   data() {
     return {
       users: [],
@@ -28,23 +33,50 @@ export default {
         pass: this.userPass,
       });
     },
+    deleteUser(index) {
+      this.users.splice(index, 1);
+    },
   },
 };
 </script>
 
 <template>
-  <input type="text" v-model="userName" placeholder="Имя" />
-  <input type="email" v-model="userEmail" placeholder="E-mail" />
-  <input type="password" v-model="userPass" placeholder="Пароль" />
-  <p className="error">{{ error }}</p>
-  <button @click="sendData()">отправить</button>
-  <div v-if="users.length == 0">Нет пользоаптелей</div>
-  <div v-else-if="users.length == 1">1 gjkmpjfdfntkm</div>
-  <div v-else>more 1 elem</div>
-  <div v-for="(el, index) in users" :key="index">
-    <span>{{ el.name }}</span> | <span>{{ el.email }}</span> |
-    <span>{{ el.pass }}</span>
+  <div class="wrapper">
+    <input type="text" v-model="userName" placeholder="Имя" />
+    <input type="email" v-model="userEmail" placeholder="E-mail" />
+    <input type="password" v-model="userPass" placeholder="Пароль" />
+    <p className="error">{{ error }}</p>
+    <button class="btn" @click="sendData()">отправить</button>
+    <div v-if="users.length == 0">Нет пользоаптелей</div>
+    <div v-else-if="users.length == 1">1 gjkmpjfdfntkm</div>
+    <div v-else>more 1 elem</div>
+    <User
+      v-for="(el, index) in users"
+      :key="index"
+      :user="el"
+      :index="index"
+      :deleteUser="deleteUser"
+    />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.wrapper input {
+  border-radius: 10px;
+  padding: 10px;
+}
+.btn {
+  background-color: #00bb00;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+    deleteUser: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <div className="user">
+    <span>{{ user.name }}</span> | <span>{{ user.email }}</span> |
+    <span>{{ user.pass }}</span>
+    <button className="danger" @click="deleteUser(index)">удалить</button>
+  </div>
+</template>
+<style scoped>
+.user {
+  background-color: antiquewhite;
+  border-radius: 10px;
+  padding: 15px;
+  display: flex;
+  gap: 10px;
+}
+.danger {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
